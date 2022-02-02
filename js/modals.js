ac35a7546d796ba7a34f739699270c1ae3983ed4
unknown <okorojiebube2@gmail.com>
2022-02-02 09:09:05 -0600

    Add dynamic data to modals.js file

--- js/modals.js
+++ js/modals.js
@@ -48,3 +48,45 @@ const projectDatas = [
 		source: null,
 	},
 ]
+
+document.querySelector('.modals').innerHTML = projectDatas.map(
+	data => `
+<div class="modal">
+<div class="modal-container"> 
+  <div class="modal-title">
+		<h2 class="project-title">${data.name}</h2>
+    <div class="close">
+		<i class="fa fa-times fa-1x"></i>
+    </div>
+	</div>
+  <ul class="project-author">
+    <li>${data.author}</li>
+    <li>${data.stack}</li>
+    <li>${data.year}</li>
+  </ul>
+  <div class="project-img">
+    <img
+      src= ${data.featuredImage}
+      class="snapshoot"
+      alt="Tonic project image"
+    />
+  </div>
+  <div class="modal-description">
+    <p class="project-text"
+      >${data.description}</p
+    >
+    <div class="modal-tools">
+      <ul class="project-tools">
+        <li>${data.technologies[0]}</li>
+        <li>${data.technologies[1]}</li>
+        <li>${data.technologies[2]}</li>
+      </ul>
+      <div class="buttons">
+        <a href="${data.liveVersion}" class="modal-button">See Live</a>
+        <a href="${data.source}" class="modal-button">See Source</a>
+      </div>
+    </div>
+  </div>
+  </div>
+  </div>`,
+)
