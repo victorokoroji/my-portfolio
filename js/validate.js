17a76885f5dc98eddb1db8b239c422fb85cd778c
unknown <okorojiebube2@gmail.com>
2022-02-03 17:48:30 -0600

    Modify email validation

--- js/validate.js
+++ js/validate.js
@@ -1,23 +1,43 @@
-function showMessage(input, message, type) {
-	// get the small element and set the message
-	const msg = input.parentNode.querySelector("small");
-	msg.innerText = message;
-	// update the class for the input
-	input.className = type ? "success" : "error";
-	return type;
+const form = document.querySelector('#form')
+const EMAIL_INVALID =
+	'Sorry!, form was not submitted. Your email should be in lowercase e.g abc@gmail.com'
+
+form.addEventListener('submit', e => {
+	e.preventDefault()
+
+	let emailValid = validateEmail(form.elements['email'], EMAIL_INVALID)
+
+	if (emailValid) {
+		form.submit()
+	}
+})
+
+function displayMessage(input, message, type) {
+	// input.parentNode.querySelector('small').innerText = message;
+	document.querySelector('small').innerText = message
+	input.className = type ? 'success' : 'error'
+	return type
 }
 
 function showError(input, message) {
-	return showMessage(input, message, false);
+	return displayMessage(input, message, false)
 }
 
 function showSuccess(input) {
-	return showMessage(input, "", true);
+	return displayMessage(input, '', true)
 }
 
 function hasValue(input, message) {
-	if (input.value.trim() === "") {
-		return showError(input, message);
+	if (input.value.trim() === '') {
+		return showError(input, message)
 	}
-	return showSuccess(input);
+	return showSuccess(input)
+}
+
+function validateEmail(input, invalidMsg) {
+	const email = input.value.trim()
+	if (email !== email.toLowerCase()) {
+		return showError(input, invalidMsg)
+	}
+	return true
 }
