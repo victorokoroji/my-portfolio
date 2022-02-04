7ebe22f22132c41f55abe17f6b14f0790e95941f
unknown <okorojiebube2@gmail.com>
2022-02-04 16:03:22 -0600

    Save data to local storage

--- js/validate.js
+++ js/validate.js
@@ -1,35 +1,53 @@
-const form = document.querySelector('#form');
-const EMAIL_INVALID = 'Sorry!, form was not submitted. Your email should be in lowercase e.g abc@gmail.com';
+const form = document.querySelector('#form')
+const EMAIL_INVALID =
+	'Sorry!, form was not submitted. Your email should be in lowercase e.g abc@gmail.com'
 
 function displayMessage(input, message, type) {
-  document.querySelector('small').innerText = message;
-  input.className = type ? 'success' : 'error';
-  return type;
+	document.querySelector('small').innerText = message
+	input.className = type ? 'success' : 'error'
+	return type
 }
 
 function showError(input, message) {
-  return displayMessage(input, message, false);
+	return displayMessage(input, message, false)
 }
 
 function showSuccess(input) {
-  return displayMessage(input, '', true);
+	return displayMessage(input, '', true)
 }
 
 function validateEmail(input, invalidMsg) {
-  const email = input.value.trim();
-  if (email !== email.toLowerCase()) {
-    return showError(input, invalidMsg);
-  }
-  showSuccess(input);
-  return true;
+	const email = input.value.trim()
+	if (email !== email.toLowerCase()) {
+		return showError(input, invalidMsg)
+	}
+	showSuccess(input)
+	return true
 }
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
+function saveData() {
+	const form = document.querySelector('#form')
+	let formData = new FormData(form)
 
-  const emailValid = validateEmail(form.elements.email, EMAIL_INVALID);
+	let existingUser = localStorage.getItem('userData')
+	existingUser = existingUser === null ? [] : JSON.parse(existingUser)
 
-  if (emailValid) {
-    form.submit();
-  }
-});
+	console.log(existingUser)
+	let currentUser = {}
+
+	for (const [key, value] of formData) {
+		currentUser[key] = value
+	}
+	existingUser[0] = currentUser
+	localStorage.setItem('userData', JSON.stringify(existingUser))
+}
+
+form.addEventListener('submit', e => {
+	e.preventDefault()
+	const emailValid = validateEmail(form.elements.email, EMAIL_INVALID)
+
+	if (emailValid) {
+		saveData()
+		form.submit()
+	}
+})
