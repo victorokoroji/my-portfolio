fec63c6f6dda480d1efb0a50cab50f9fabf17727
unknown <okorojiebube2@gmail.com>
2022-02-05 16:03:16 -0600

    Fix linter errors

--- js/data.js
+++ js/data.js
@@ -4,8 +4,7 @@ const projectDatas = [
     author: 'CANOPY',
     stack: 'Backend Dev',
     year: 2015,
-    description:
-			'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     featuredImage: './assets/images/Snapshoot-Portfolio-3.png',
     technologies: ['html', 'css', 'javascript'],
     liveVersion: null,
@@ -16,8 +15,7 @@ const projectDatas = [
     author: 'FACEBOOK',
     stack: 'Full Stack Dev',
     year: 2015,
-    description:
-			'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+    description: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
     featuredImage: './assets/images/Snapshoot-Portfolio-4.png',
     technologies: ['html', 'css', 'javascript'],
     liveVersion: null,
@@ -28,8 +26,7 @@ const projectDatas = [
     author: 'FACEBOOK 360',
     stack: 'Full Stack Dev',
     year: 2015,
-    description:
-			"Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
+    description: "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
     featuredImage: './assets/images/Snapshoot-Portfolio.png',
     technologies: ['html', 'css', 'javascript'],
     liveVersion: null,
@@ -40,8 +37,7 @@ const projectDatas = [
     author: 'Uber',
     stack: 'Lead Developer',
     year: 2015,
-    description:
-			"Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
+    description: "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
     featuredImage: './assets/images/Snapshoot-Portfolio-2.png',
     technologies: ['html', 'css', 'javascript', 'bootstrap'],
     liveVersion: null,
@@ -53,39 +49,38 @@ document.querySelector('.works').innerHTML = projectDatas
   .map(
     (data) => `
     <section class="project">
-				<div class="project-img">
-					<img
-						src= ${data.featuredImage}
-						class="snapshoot"
-						alt="Tonic project image"
-					/>
-				</div>
-				<div class="project-description">
-					<h2 class="project-title">${data.name}</h2>
-					<ul class="project-author">
-						<li>${data.author}</li>
-            <li>${data.stack}</li>
-            <li>${data.year}</li>
-					</ul>
-					<p class="project-text"
-						>${data.description}</p
-					>
-					<ul class="project-tools">
-						<li>${data.technologies[0]}</li>
-            <li>${data.technologies[1]}</li>
-            <li>${data.technologies[2]}</li>
-            <li>${data.technologies[3]}</li>
-					</ul>
-					<button type="button" class="btn">See Project</button>
-				</div>
-			</section>
+<div class="project-img">
+<img
+src= ${data.featuredImage}
+class="snapshoot"
+alt="Tonic project image"
+/>
+</div>
+<div class="project-description">
+<h2 class="project-title">${data.name}</h2>
+<ul class="project-author">
+<li>${data.author}</li>
+<li>${data.stack}</li>
+<li>${data.year}</li>
+</ul>
+<p class="project-text"
+>${data.description}</p
+>
+<ul class="project-tools">
+<li>${data.technologies[0]}</li>
+<li>${data.technologies[1]}</li>
+<li>${data.technologies[2]}</li>
+<li>${data.technologies[3]}</li>
+</ul>
+<button type="button" class="btn">See Project</button>
+</div>
+</section>
     `,
   )
   .join('');
 
-for (let i = 0; i <= projectDatas.length; i++) {
-  if (i % 2 == 1) {
-    console.log(i);
+for (let i = 0; i <= projectDatas.length; i += 1) {
+  if (i % 2 === 1) {
     document.querySelectorAll('.project')[i].classList.add('order');
   }
 }
