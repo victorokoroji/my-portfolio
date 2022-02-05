884535de807eb6af6b4f081d2a6e961cbc961223
unknown <okorojiebube2@gmail.com>
2022-02-04 19:06:23 -0600

    Fix linter errors

--- js/formData.js
+++ js/formData.js
@@ -1,38 +1,38 @@
 const formData = {
-	name: '',
-	email: '',
-	description: '',
-}
+  name: '',
+  email: '',
+  description: '',
+};
 
 function fillDatas() {
-	let storageData = localStorage.getItem('form-data')
-	storageData = JSON.parse(storageData)
-	document.getElementById('name').value = storageData.name
-	document.getElementById('email').value = storageData.email
-	document.getElementById('description').value = storageData.description
+  let storageData = localStorage.getItem('form-data');
+  storageData = JSON.parse(storageData);
+  document.getElementById('name').value = storageData.name;
+  document.getElementById('email').value = storageData.email;
+  document.getElementById('description').value = storageData.description;
 }
 
 function populateDatas() {
-	formData.name = document.getElementById('name').value
-	formData.email = document.getElementById('email').value
-	formData.description = document.getElementById('description').value
-	localStorage.setItem('form-data', JSON.stringify(formData))
-	fillDatas()
+  formData.name = document.getElementById('name').value;
+  formData.email = document.getElementById('email').value;
+  formData.description = document.getElementById('description').value;
+  localStorage.setItem('form-data', JSON.stringify(formData));
+  fillDatas();
 }
 
-const inputList = []
-inputList.push(document.getElementById('name'))
-inputList.push(document.getElementById('email'))
-inputList.push(document.getElementById('description'))
+const inputList = [];
+inputList.push(document.getElementById('name'));
+inputList.push(document.getElementById('email'));
+inputList.push(document.getElementById('description'));
 
-inputList.forEach(value => {
-	value.onchange = function trig() {
-		populateDatas()
-	}
-})
+inputList.forEach((value) => {
+  value.onchange = function trig() {
+    populateDatas();
+  };
+});
 
 if (!localStorage.getItem('form-data')) {
-	populateDatas()
+  populateDatas();
 } else {
-	fillDatas()
+  fillDatas();
 }
