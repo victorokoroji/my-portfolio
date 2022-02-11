be83369393de56cd8460d456b6b318b05922833c
unknown <okorojiebube2@gmail.com>
2022-02-11 13:42:59 -0600

    Modify mobile menu styling

--- js/data.js
+++ js/data.js
@@ -1,49 +1,53 @@
 const projectDatas = [
-  {
-    name: 'Tonic',
-    author: 'CANOPY',
-    stack: 'Backend Dev',
-    year: 2015,
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    featuredImage: './assets/images/Snapshoot-Portfolio-3.png',
-    technologies: ['html', 'css', 'javascript'],
-    liveVersion: null,
-    source: null,
-  },
-  {
-    name: 'Multi-Post Stories',
-    author: 'FACEBOOK',
-    stack: 'Full Stack Dev',
-    year: 2015,
-    description: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
-    featuredImage: './assets/images/Snapshoot-Portfolio-4.png',
-    technologies: ['html', 'css', 'javascript'],
-    liveVersion: null,
-    source: null,
-  },
-  {
-    name: 'Tonic',
-    author: 'FACEBOOK 360',
-    stack: 'Full Stack Dev',
-    year: 2015,
-    description: "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
-    featuredImage: './assets/images/Snapshoot-Portfolio.png',
-    technologies: ['html', 'css', 'javascript'],
-    liveVersion: null,
-    source: null,
-  },
-  {
-    name: 'Uber Navigation',
-    author: 'Uber',
-    stack: 'Lead Developer',
-    year: 2015,
-    description: "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
-    featuredImage: './assets/images/Snapshoot-Portfolio-2.png',
-    technologies: ['html', 'css', 'javascript', 'bootstrap'],
-    liveVersion: null,
-    source: null,
-  },
-];
+	{
+		name: 'Tonic',
+		author: 'CANOPY',
+		stack: 'Backend Dev',
+		year: 2015,
+		description:
+			'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+		featuredImage: './assets/images/Snapshoot-Portfolio-3.png',
+		technologies: ['html', 'css', 'javascript', 'bootstrap'],
+		liveVersion: null,
+		source: null,
+	},
+	{
+		name: 'Multi-Post Stories',
+		author: 'FACEBOOK',
+		stack: 'Full Stack Dev',
+		year: 2015,
+		description:
+			'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+		featuredImage: './assets/images/Snapshoot-Portfolio-4.png',
+		technologies: ['html', 'css', 'javascript', 'bootstrap'],
+		liveVersion: null,
+		source: null,
+	},
+	{
+		name: 'Tonic',
+		author: 'FACEBOOK 360',
+		stack: 'Full Stack Dev',
+		year: 2015,
+		description:
+			"Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
+		featuredImage: './assets/images/Snapshoot-Portfolio.png',
+		technologies: ['html', 'css', 'javascript', 'bootstrap'],
+		liveVersion: null,
+		source: null,
+	},
+	{
+		name: 'Uber Navigation',
+		author: 'Uber',
+		stack: 'Lead Developer',
+		year: 2015,
+		description:
+			"Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
+		featuredImage: './assets/images/Snapshoot-Portfolio-2.png',
+		technologies: ['html', 'css', 'javascript', 'bootstrap'],
+		liveVersion: null,
+		source: null,
+	},
+]
 
 document.querySelector('.works').innerHTML = projectDatas
   .map(
