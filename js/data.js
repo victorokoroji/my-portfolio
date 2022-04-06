8e6a188f07b1ce80e42ad2a4391a9a4c5e65659d
unknown <okorojiebube2@gmail.com>
2022-04-06 13:29:30 -0600

    Add scotland adventure  description

--- js/data.js
+++ js/data.js
@@ -1,5 +1,17 @@
 const projectDatas = [
 	{
+		name: 'Scotland Adventure',
+		author: 'OKOROJI VICTOR',
+		stack: 'Frontend Dev',
+		year: 2022,
+		description:
+			'This is a story telling web application that provides a platform for story tellers who want to share and document their adventures about several locations in Scotland, to post stories which may include text, photos, and multimedia about their experience.',
+		featuredImage: './assets/images/scotland.png',
+		technologies: ['html', 'css', 'javascript'],
+		liveVersion: 'https://scotlandadventureapp.herokuapp.com',
+		source: 'https://github.com/vickymarz/scotland-adventures',
+	},
+	{
 		name: 'Math-Magician',
 		author: 'OKOROJI VICTOR',
 		stack: 'Frontend Dev',
@@ -31,18 +43,6 @@ const projectDatas = [
 		description:
 			"Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
 		featuredImage: './assets/images/Snapshoot-Portfolio.png',
-		technologies: ['html', 'css', 'javascript'],
-		liveVersion: null,
-		source: null,
-	},
-	{
-		name: 'Uber Navigation',
-		author: 'Uber',
-		stack: 'Lead Developer',
-		year: 2015,
-		description:
-			"Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
-		featuredImage: './assets/images/Snapshoot-Portfolio-2.png',
 		technologies: ['html', 'css', 'javascript'],
 		liveVersion: null,
 		source: null,
