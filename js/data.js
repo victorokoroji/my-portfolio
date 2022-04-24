03507086ca82b771540771320c71eef618578de3
unknown <okorojiebube2@gmail.com>
2022-04-24 07:28:37 -0600

    Fix linter errors

--- js/data.js
+++ js/data.js
@@ -1,81 +1,75 @@
 const projectDatas = [
-	{
-		name: 'Scotland Adventure',
-		author: 'OKOROJI VICTOR',
-		stack: 'Frontend Dev',
-		year: 2022,
-		description:
-			'This is a story telling web application that provides a platform for story tellers who want to share and document their adventures about several locations in Scotland, to post stories which may include text, photos, and multimedia about their experience.',
-		featuredImage: './assets/images/scotland.png',
-		technologies: ['html', 'css', 'javascript'],
-		liveVersion: 'https://scotlandadventureapp.herokuapp.com',
-		source: 'https://github.com/vickymarz/scotland-adventures',
-	},
-	{
-		name: 'Space Travelers Hub',
-		author: 'OKOROJI VICTOR',
-		stack: 'Frontend Dev',
-		year: 2022,
-		description:
-			'Space Travelers Hub is a web application made for a company that provides commercial and scientific space travel services. The application allows users (future clients) to book rockets and join selected space missions.',
-		featuredImage: '../assets/images/Screen-shoot.gif',
-		technologies: ['React', 'Redux', 'CSS'],
-		liveVersion: 'https://space-travellers-booking-app.herokuapp.com/',
-		source: 'https://github.com/vickymarz/space-travellers-hub',
-	},
-	{
-		name: 'Todolist App',
-		author: 'OKOROJI VICTOR',
-		stack: 'Frontend Dev',
-		year: 2022,
-		description:
-			'"To-do list" is a tool that helps to organize your day. It simply lists the things that you need to do and allows you to mark them as complete.',
-		featuredImage: './assets/images/todolist.png',
-		technologies: ['html', 'css', 'javascript'],
-		liveVersion: 'https://vickymarz.github.io/todo-list/dist/',
-		source: 'https://github.com/vickymarz/todo-list',
-	},
-	{
-		name: 'Math-Magician',
-		author: 'OKOROJI VICTOR',
-		stack: 'Frontend Dev',
-		year: 2022,
-		description:
-			'Math-Magician" is a web app for all lovers of mathematics. It is a Single Page Application that allows users to make simple calculations.',
-		featuredImage: './assets/images/calculator.png',
-		technologies: ['React', 'css', 'jest'],
-		liveVersion: 'https://math-magician-calculator.netlify.app/',
-		source: 'https://github.com/vickymarz/math-magician',
-	},
-	{
-		name: 'Bookstore',
-		author: 'OKOROJI VICTOR',
-		stack: 'Frontend Dev',
-		year: 2022,
-		description:
-			"Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
-		featuredImage: '../assets/images/bookstore.png',
-		technologies: ['Redux', 'React', 'CSS'],
-		liveVersion: 'https://mybookstorewebapp.herokuapp.com/',
-		source: 'https://github.com/vickymarz/bookstore',
-	},
-	{
-		name: 'Portfolio',
-		author: 'OKOROJI VICTOR',
-		stack: 'Frontend Dev',
-		year: 2022,
-		description:
-			'This is my Portfolio website which keeps track of all the projects I have worked on, including projects live versions and link to the code. This is a way for me to show what I can do as a software developer.',
-		featuredImage: '../assets/images/portfolio.png',
-		technologies: ['HTML', 'CSS', 'Javascript'],
-		liveVersion: 'https://victorokoroji.netlify.app/',
-		source: 'https://github.com/vickymarz/my-portfolio',
-	},
-]
+  {
+    name: 'Scotland Adventure',
+    author: 'OKOROJI VICTOR',
+    stack: 'Frontend Dev',
+    year: 2022,
+    description: 'This is a story telling web application that provides a platform for story tellers who want to share and document their adventures about several locations in Scotland, to post stories which may include text, photos, and multimedia about their experience.',
+    featuredImage: './assets/images/scotland.png',
+    technologies: ['html', 'css', 'javascript'],
+    liveVersion: 'https://scotlandadventureapp.herokuapp.com',
+    source: 'https://github.com/vickymarz/scotland-adventures',
+  },
+  {
+    name: 'Space Travelers Hub',
+    author: 'OKOROJI VICTOR',
+    stack: 'Frontend Dev',
+    year: 2022,
+    description: 'Space Travelers Hub is a web application made for a company that provides commercial and scientific space travel services. The application allows users (future clients) to book rockets and join selected space missions.',
+    featuredImage: '../assets/images/Screen-shoot.gif',
+    technologies: ['React', 'Redux', 'CSS'],
+    liveVersion: 'https://space-travellers-booking-app.herokuapp.com/',
+    source: 'https://github.com/vickymarz/space-travellers-hub',
+  },
+  {
+    name: 'Todolist App',
+    author: 'OKOROJI VICTOR',
+    stack: 'Frontend Dev',
+    year: 2022,
+    description: '"To-do list" is a tool that helps to organize your day. It simply lists the things that you need to do and allows you to mark them as complete.',
+    featuredImage: './assets/images/todolist.png',
+    technologies: ['html', 'css', 'javascript'],
+    liveVersion: 'https://vickymarz.github.io/todo-list/dist/',
+    source: 'https://github.com/vickymarz/todo-list',
+  },
+  {
+    name: 'Math-Magician',
+    author: 'OKOROJI VICTOR',
+    stack: 'Frontend Dev',
+    year: 2022,
+    description: 'Math-Magician" is a web app for all lovers of mathematics. It is a Single Page Application that allows users to make simple calculations.',
+    featuredImage: './assets/images/calculator.png',
+    technologies: ['React', 'css', 'jest'],
+    liveVersion: 'https://math-magician-calculator.netlify.app/',
+    source: 'https://github.com/vickymarz/math-magician',
+  },
+  {
+    name: 'Bookstore',
+    author: 'OKOROJI VICTOR',
+    stack: 'Frontend Dev',
+    year: 2022,
+    description: "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
+    featuredImage: '../assets/images/bookstore.png',
+    technologies: ['Redux', 'React', 'CSS'],
+    liveVersion: 'https://mybookstorewebapp.herokuapp.com/',
+    source: 'https://github.com/vickymarz/bookstore',
+  },
+  {
+    name: 'Portfolio',
+    author: 'OKOROJI VICTOR',
+    stack: 'Frontend Dev',
+    year: 2022,
+    description: 'This is my Portfolio website which keeps track of all the projects I have worked on, including projects live versions and link to the code. This is a way for me to show what I can do as a software developer.',
+    featuredImage: '../assets/images/portfolio.png',
+    technologies: ['HTML', 'CSS', 'Javascript'],
+    liveVersion: 'https://victorokoroji.netlify.app/',
+    source: 'https://github.com/vickymarz/my-portfolio',
+  },
+];
 
 document.querySelector('.works').innerHTML = projectDatas
-	.map(
-		data => `
+  .map(
+    (data) => `
     <section class="project">
 <div class="project-img">
 <img
@@ -103,83 +97,74 @@ alt="Tonic project image"
 </div>
 </section>
     `,
-	)
-	.join('')
+  )
+  .join('');
 
 for (let i = 0; i <= projectDatas.length; i += 1) {
-	if (i % 2 === 1) {
-		document.querySelectorAll('.project')[i].classList.add('order')
-	}
+  if (i % 2 === 1) {
+    document.querySelectorAll('.project')[i].classList.add('order');
+  }
 }
 
 document.querySelector('.modals').innerHTML = projectDatas
-	.map(
-		data => `
+  .map(
+    (data) => `
 <div class="modal">
 <div class="modal-container">
-  <div class="modal-title">
+<div class="modal-title">
 <h2 class="project-title">${data.name}</h2>
-    <div class="close">
-  <i class="fa fa-times fa-1x"></i>
-    </div>
+<div class="close">
+<i class="fa fa-times fa-1x"></i>
 </div>
-  <ul class="project-author">
-    <li>${data.author}</li>
-    <li>${data.stack}</li>
-    <li>${data.year}</li>
-  </ul>
-  <div>
-    <img
-      src= ${data.featuredImage}
-      class="snapshoot"
-      alt="Tonic project image"
-    />
-  </div>
-  <div class="modal-description">
-    <p class="project-text"
-      >${data.description}</p
-    >
-    <div class="modal-tools">
-      <ul class="project-tools">
-        <li>${data.technologies[0]}</li>
-        <li>${data.technologies[1]}</li>
-        <li>${data.technologies[2]}</li>
-      </ul>
-      <span></span>
-      <div class="buttons">
-        <a href="${data.liveVersion}" class="modal-button" target="_blank" rel="noopener noreferrer">See Live <img
-src="./assets/images/Icon.png"
-alt="live site icon" class="modal-img"
-/></a>
-        <a href="${data.source}" class="modal-button" target="_blank" rel="noopener noreferrer">See Source <img
-src="./assets/images/github.png"
-alt="github icon" class="modal-img"
-/></a>
-      </div>
-    </div>
-  </div>
-  </div>
-  </div>`,
-	)
-	.join('')
+</div>
+<ul class="project-author">
+<li>${data.author}</li>
+<li>${data.stack}</li>
+<li>${data.year}</li>
+</ul>
+<div>
+<img
+src= ${data.featuredImage}
+class="snapshoot"
+alt="Tonic project image"
+/>
+</div>
+<div class="modal-description">
+<p class="project-text">${data.description}</p>
+<div class="modal-tools">
+<ul class="project-tools">
+<li>${data.technologies[0]}</li>
+<li>${data.technologies[1]}</li>
+<li>${data.technologies[2]}</li>
+</ul>
+<span></span>
+<div class="buttons">
+<a href="${data.liveVersion}" class="modal-button" target="_blank" rel="noopener noreferrer">See Live <img src="./assets/images/Icon.png" alt="live site icon" class="modal-img"/></a>
+<a href="${data.source}" class="modal-button" target="_blank" rel="noopener noreferrer">See Source <img src="./assets/images/github.png" alt="github icon" class="modal-img"/></a>
+</div>
+</div>
+</div>
+</div>
+</div>`,
+  )
+  .join('');
 
 // event listeners for opening modal on button click
-const modalButtons = Array.from(document.querySelectorAll('.btn'))
-const modals = Array.from(document.querySelectorAll('.modal'))
+const modalButtons = Array.from(document.querySelectorAll('.btn'));
+const modals = Array.from(document.querySelectorAll('.modal'));
 
-const modalButtonZip = modalButtons.map((button, i) => [button, modals[i]])
-modalButtonZip.forEach(pair => {
-	pair[0].addEventListener('click', () => {
-		pair[1].style.display = 'block'
-	})
-})
+const modalButtonZip = modalButtons.map((button, i) => [button, modals[i]]);
+modalButtonZip.forEach((pair) => {
+  pair[0].addEventListener('click', () => {
+    pair[1].style.display = 'block';
+  });
+});
 
 // event listener for closing modal on button click
-document.querySelectorAll('.close').forEach(close => {
-	close.addEventListener('click', () => {
-		document.querySelectorAll('.modal').forEach(modal => {
-			modal.style.display = 'none'
-		})
-	})
-})
-
+document.querySelectorAll('.close').forEach((close) => {
+  close.addEventListener('click', () => {
+    document.querySelectorAll('.modal').forEach((modal) => {
+      modal.style.display = 'none';
+    });
+  });
+});
--- js/index.js
+++ js/index.js
@@ -1,122 +1,120 @@
 function toggleMenu() {
-	document.querySelectorAll('.menu-bar').forEach(element =>
-		element.addEventListener('click', () => {
-			document.querySelector('.mobile-menu').classList.toggle('active')
-		}),
-	)
+  document.querySelectorAll('.menu-bar').forEach((element) => element.addEventListener('click', () => {
+    document.querySelector('.mobile-menu').classList.toggle('active');
+  }));
 }
 
-toggleMenu()
+toggleMenu();
 
 function introSectionObserver() {
-	const header = document.querySelector('header')
-	const introSection = document.querySelector('.divider')
+  const header = document.querySelector('header');
+  const introSection = document.querySelector('.divider');
 
-	const options = {
-		rootMargin: '0px',
-	}
+  const options = {
+    rootMargin: '0px',
+  };
 
-	const observer = new IntersectionObserver(entries => {
-		entries.forEach(entry => {
-			if (!entry.isIntersecting) {
-				header.classList.add('header-active')
-			} else {
-				header.classList.remove('header-active')
-			}
-		})
-	}, options)
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) {
+        header.classList.add('header-active');
+      } else {
+        header.classList.remove('header-active');
+      }
+    });
+  }, options);
 
-	observer.observe(introSection)
+  observer.observe(introSection);
 }
-introSectionObserver()
+introSectionObserver();
 
 function portfolioSectionObserver() {
-	const portfolioNavlink = document.querySelector('#portfolio-link')
-	const portfolioSection = document.querySelector('#portfolio')
+  const portfolioNavlink = document.querySelector('#portfolio-link');
+  const portfolioSection = document.querySelector('#portfolio');
 
-	const options = {
-		rootMargin: '-280px',
-	}
+  const options = {
+    rootMargin: '-280px',
+  };
 
-	const observer = new IntersectionObserver(entries => {
-		entries.forEach(entry => {
-			if (entry.isIntersecting) {
-				portfolioNavlink.classList.add('active-link')
-			} else {
-				portfolioNavlink.classList.remove('active-link')
-			}
-		})
-	}, options)
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        portfolioNavlink.classList.add('active-link');
+      } else {
+        portfolioNavlink.classList.remove('active-link');
+      }
+    });
+  }, options);
 
-	observer.observe(portfolioSection)
+  observer.observe(portfolioSection);
 }
-portfolioSectionObserver()
+portfolioSectionObserver();
 
 function aboutSectionObserver() {
-	const aboutNavlink = document.querySelector('#about-link')
-	const aboutSection = document.querySelector('#about')
+  const aboutNavlink = document.querySelector('#about-link');
+  const aboutSection = document.querySelector('#about');
 
-	const options = {
-		rootMargin: '-280px',
-	}
+  const options = {
+    rootMargin: '-280px',
+  };
 
-	const observer = new IntersectionObserver(entries => {
-		entries.forEach(entry => {
-			if (entry.isIntersecting) {
-				aboutNavlink.classList.add('active-link')
-			} else {
-				aboutNavlink.classList.remove('active-link')
-			}
-		})
-	}, options)
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        aboutNavlink.classList.add('active-link');
+      } else {
+        aboutNavlink.classList.remove('active-link');
+      }
+    });
+  }, options);
 
-	observer.observe(aboutSection)
+  observer.observe(aboutSection);
 }
-aboutSectionObserver()
+aboutSectionObserver();
 
 function contactSectionObserver() {
-	const contactNavlink = document.querySelector('#contact-link')
-	const contactSection = document.querySelector('#contact')
+  const contactNavlink = document.querySelector('#contact-link');
+  const contactSection = document.querySelector('#contact');
 
-	const options = {
-		rootMargin: '-280px',
-	}
+  const options = {
+    rootMargin: '-280px',
+  };
 
-	const observer = new IntersectionObserver(entries => {
-		entries.forEach(entry => {
-			if (entry.isIntersecting) {
-				contactNavlink.classList.add('active-link')
-			} else {
-				contactNavlink.classList.remove('active-link')
-			}
-		})
-	}, options)
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        contactNavlink.classList.add('active-link');
+      } else {
+        contactNavlink.classList.remove('active-link');
+      }
+    });
+  }, options);
 
-	observer.observe(contactSection)
+  observer.observe(contactSection);
 }
-contactSectionObserver()
+contactSectionObserver();
 
 function toggleArrows() {
-	const lists = Array.from(document.querySelectorAll('.lists'))
-	const angleIcon = Array.from(document.querySelectorAll('.angle-icon'))
+  const lists = Array.from(document.querySelectorAll('.lists'));
+  const angleIcon = Array.from(document.querySelectorAll('.angle-icon'));
 
-	const listZip = angleIcon.map((btn, index) => [btn, lists[index]])
+  const listZip = angleIcon.map((btn, index) => [btn, lists[index]]);
 
-	listZip.forEach(pair => {
-    pair[1].style.display = 'block'
+  listZip.forEach((pair) => {
+    pair[1].style.display = 'block';
 
-		pair[0].addEventListener('click', () => {
-			if (pair[1].style.display == 'block' || pair[1].style.display == 'flex') {
-				pair[1].style.display = 'none'
-				pair[0].classList.add('rotate-angle-icon')
-			} else if (window.innerWidth > 992) {
-				pair[1].style.display = 'flex'
-				pair[0].classList.remove('rotate-angle-icon')
-			} else  {
-				pair[1].style.display = 'block'
-				pair[0].classList.remove('rotate-angle-icon')
+    pair[0].addEventListener('click', () => {
+      if (pair[1].style.display === 'block' || pair[1].style.display === 'flex') {
+        pair[1].style.display = 'none';
+        pair[0].classList.add('rotate-angle-icon');
+      } else if (window.innerWidth > 992) {
+        pair[1].style.display = 'flex';
+        pair[0].classList.remove('rotate-angle-icon');
+      } else {
+        pair[1].style.display = 'block';
+        pair[0].classList.remove('rotate-angle-icon');
       }
-		})
-	})
+    });
+  });
 }
-toggleArrows()
+toggleArrows();
