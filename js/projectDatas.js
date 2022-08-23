e32bf21e2dc354a6a19ba84c515eee0c5d048f3b
vickymarz <okorojiebube2@gmail.com>
2022-08-23 22:03:11 +0100

    Add doctors appointment app

--- js/projectDatas.js
+++ js/projectDatas.js
@@ -2,7 +2,7 @@ const projectDatas = [
   {
     name: 'Scotland Adventure',
     author: 'OKOROJI VICTOR',
-    stack: 'Frontend Dev',
+    stack: 'Full-Stack Dev',
     year: 2022,
     description: 'This is a story telling web application that provides a platform for story tellers who want to share and document their adventures about several locations in Scotland, to post stories which may include text, photos, and multimedia about their experience.',
     featuredImage: './assets/images/scotland.png',
@@ -11,9 +11,20 @@ const projectDatas = [
     source: 'https://github.com/vickymarz/scotland-adventures',
   },
   {
+    name: 'Always Care',
+    author: 'OKOROJI VICTOR',
+    stack: 'Full-Stack Dev',
+    year: 2022,
+    description: 'Alwayscare" doctors appointment app is a software application that is especially meant to help patients manage and schedule appointments with the doctors or opt for a real-time chat.',
+    featuredImage: './assets/images/alwayscare.png',
+    technologies: ['React', 'Redux', 'Ruby on Rails'],
+    liveVersion: 'https://alwayscare.herokuapp.com/',
+    source: 'https://github.com/vickymarz/doctors-appointment-frontend',
+  },
+  {
     name: 'Space Travelers Hub',
     author: 'OKOROJI VICTOR',
-    stack: 'Frontend Dev',
+    stack: 'Full-Stack Dev',
     year: 2022,
     description: 'Space Travelers Hub is a web application made for a company that provides commercial and scientific space travel services. The application allows users (future clients) to book rockets and join selected space missions.',
     featuredImage: '../assets/images/Screen-shoot.gif',
@@ -24,7 +35,7 @@ const projectDatas = [
   {
     name: 'Todolist App',
     author: 'OKOROJI VICTOR',
-    stack: 'Frontend Dev',
+    stack: 'Full-Stack Dev',
     year: 2022,
     description: '"To-do list" is a tool that helps to organize your day. It simply lists the things that you need to do and allows you to mark them as complete.',
     featuredImage: './assets/images/todolist.png',
@@ -35,7 +46,7 @@ const projectDatas = [
   {
     name: 'Awesome Books',
     author: 'OKOROJI VICTOR',
-    stack: 'Frontend Dev',
+    stack: 'Full-Stack Dev',
     year: 2022,
     description: 'This website is dedicated for users to be able to store there collection of books they would love to read or the books they are reading. They can add new books and delete existing books with ease.',
     featuredImage: './assets/images/awesomebooks.png',
@@ -46,7 +57,7 @@ const projectDatas = [
   {
     name: 'Calculator App',
     author: 'OKOROJI VICTOR',
-    stack: 'Frontend Dev',
+    stack: 'Full-Stack Dev',
     year: 2022,
     description: 'Calculator App" is a web app for all lovers of mathematics. It is a Single Page Application that allows users to make simple calculations.',
     featuredImage: './assets/images/calculator.png',
@@ -57,7 +68,7 @@ const projectDatas = [
   {
     name: 'Makeup Data App',
     author: 'OKOROJI VICTOR',
-    stack: 'Frontend Dev',
+    stack: 'Full-Stack Dev',
     year: 2022,
     description: 'This application shows information about a makeup product called "Foundation". It is built around an API to fetch the needed data. It can show information on a particular brand and type of foundations. This app helps to promote the business of makeups and enable users to select there desired makeup effortlessly.',
     featuredImage: '../assets/images/makeup.gif',
@@ -68,7 +79,7 @@ const projectDatas = [
   {
     name: 'Bookstore',
     author: 'OKOROJI VICTOR',
-    stack: 'Frontend Dev',
+    stack: 'Full-Stack Dev',
     year: 2022,
     description: 'This is a React application for managing books you want to read. It tracks your progress and makes the reading the books enlisted as a goal.',
     featuredImage: '../assets/images/bookstore.png',
@@ -79,7 +90,7 @@ const projectDatas = [
   {
     name: 'Portfolio',
     author: 'OKOROJI VICTOR',
-    stack: 'Frontend Dev',
+    stack: 'Full-Stack Dev',
     year: 2022,
     description: 'This is my Portfolio website built with HTML, CSS, and Javascript. This website keeps track of all the projects I have worked on, including projects live versions and link to the code. This is a way for me to show what I can do as a software developer.',
     featuredImage: '../assets/images/portfolio.png',
